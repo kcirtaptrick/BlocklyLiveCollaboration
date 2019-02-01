51b9cab94f3b0d2d206a922e60328d328b383335
Patrick Wang <patrick.bohan.wang@gmail.com>
2019-02-01 17:10:09 +0000

    Working simple test

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 var express = require('express');
-var app = express;
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(process.env.PORT);
+app.set('view engine', 'pug')
+
+app.use(express.static('/home/ubuntu/workspace/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile('/home/ubuntu/workspace/views/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('test', {test: 'Works'})
+  socket.on('Update Text', function(data) {
+    
+  });
+  socket.on('Update Blocks', function (data) {
+    console.log();
+  });
+  console.log("Connection");
+});
